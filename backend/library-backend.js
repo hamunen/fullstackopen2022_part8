644213d980b0d069ef000d482554d6de866f3328
Henri <henri.hamunen@gmail.com>
2023-02-18 16:59:56 +0200

    ex 8.3, add nodemon

--- backend/library-backend.js
+++ backend/library-backend.js
@@ -105,10 +105,16 @@ const typeDefs = `
     genres: [String!]
   }
 
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]
+    allAuthors: [Author!]
   }
 `
 
@@ -117,6 +123,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (author) =>
+      books.filter((book) => book.author === author.name).length,
   },
 }
 
