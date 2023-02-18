51a216fdbaac9d3dffcdedaa43cac154482808fd
Henri <henri.hamunen@gmail.com>
2023-02-18 21:12:51 +0200

    ex 8.12 - authors birthyear advanced

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -2,10 +2,20 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import { Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const padding = {
     padding: 5,
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
   }
 
   return (
@@ -21,15 +31,23 @@ const App = () => {
           add book
         </Link>
       </div>
+      <Notify errorMessage={errorMessage} />
 
       <Routes>
         <Route path='/' element={<Authors />} />
-        <Route path='/authors' element={<Authors />} />
+        <Route path='/authors' element={<Authors setError={notify} />} />
         <Route path='/books' element={<Books />} />
         <Route path='/add' element={<NewBook />} />
       </Routes>
     </div>
   )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+  return <div style={{ color: 'red' }}>{errorMessage}</div>
 }
 
 export default App
--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,8 +1,9 @@
 import { useQuery, useMutation } from '@apollo/client'
 import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
 import { useState } from 'react'
+import Select from 'react-select'
 
-const Authors = () => {
+const Authors = ({ setError }) => {
   const result = useQuery(ALL_AUTHORS)
 
   if (result.loading) {
@@ -29,17 +30,28 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
-      <BirthYearForm />
+      <BirthYearForm authorNames={authors.map((a) => a.name)} setError />
     </div>
   )
 }
 
-const BirthYearForm = () => {
+const BirthYearForm = ({ authorNames, setError }) => {
   const [name, setName] = useState('')
   const [born, setBorn] = useState('')
+  const isValid = name !== '' && born !== ''
+
   const [editAuthor] = useMutation(EDIT_AUTHOR, {
     refetchQueries: [{ query: ALL_AUTHORS }],
+    onError: (error) => {
+      const errors = error.graphQLErrors[0].extensions.error.errors
+      const messages = Object.values(errors)
+        .map((e) => e.message)
+        .join('\n')
+      setError(messages)
+    },
   })
+
+  const options = authorNames.map((a) => ({ value: a, label: a }))
 
   const submit = async (event) => {
     event.preventDefault()
@@ -51,17 +63,16 @@ const BirthYearForm = () => {
     setName('')
     setBorn('')
   }
+
   return (
     <div>
       <h3>Set birthyear</h3>
-      <form onSubmit={submit}>
-        <div>
-          name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
+      <form style={{ width: '300px' }} onSubmit={submit}>
+        <Select
+          placeholder={'Select author'}
+          options={options}
+          onChange={(e) => setName(e.value)}
+        />
         <div>
           born
           <input
@@ -70,7 +81,9 @@ const BirthYearForm = () => {
             onChange={({ target }) => setBorn(target.value)}
           />
         </div>
-        <button type='submit'>update author</button>
+        <button type='submit' disabled={!isValid}>
+          update author
+        </button>
       </form>
     </div>
   )
