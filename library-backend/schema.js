fc13de8983b03360c05fdabe92a96657f5650c15
Henri Hamunen <henri.hamunen@sievo.com>
2023-02-25 14:00:29 +0200

    Refactor backend

---
+++ library-backend/schema.js
@@ -0,0 +1,57 @@
+const typeDefs = `
+  type Book {
+    title: String!
+    author: Author!
+    published: Int!
+    genres: [String!]
+  }
+
+  type Author {
+    name: String!
+    bookCount: Int!
+    born: Int
+  }
+
+  type User {
+    username: String!
+    favouriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]
+    allAuthors: [Author!]
+    me: User
+    myRecommendations: [Book!]
+    allGenres: [String!]
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]
+    ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+    createUser(
+      username: String!
+      favouriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+  }
+`
+
+module.exports = typeDefs
